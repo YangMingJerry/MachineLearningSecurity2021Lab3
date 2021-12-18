eb245c4d57c656fd924a3f7d4651a5287c656e47
ming <my2153@nyu.edu>
2021-12-17 23:21:57 -0500

    perturbation exp better result

--- perturbation.py
+++ perturbation.py
@@ -5,22 +5,48 @@ from matplotlib import pyplot as plt
 
 from eval import data_loader
 
+def eval():
+    threshold = 0.999999
+    labels = bd_model.predict(cl_x_test)
+    labels_raw = bd_model.predict(cl_x_test_raw)
+    cl_label_p = np.argmax(labels_raw, axis=1)
+    for i,label in enumerate(labels):
+        if max(label) > threshold:
+            cl_label_p[i] = 1283
+    clean_accuracy = np.mean(np.equal(cl_label_p, cl_y_test)) * 100
+    print('Clean Classification accuracy:', clean_accuracy)
+
+
+    labels = bd_model.predict(bd_x_test)
+    labels_raw = bd_model.predict(bd_x_test_raw)
+    bd_label_p = np.argmax(labels_raw, axis=1)
+    for i,label in enumerate(labels):
+        if max(label) > threshold:
+            bd_label_p[i] = 1283
+    asr = np.mean(np.equal(bd_label_p, bd_y_test)) * 100
+    print('Attack Success Rate:', asr)
+
 if __name__ == '__main__':
     clean_data_filename = 'E:\pycharmProjects\CSAW-HackML-2020\lab3\data\cl\\valid.h5'
     poisoned_data_filename = 'E:\pycharmProjects\CSAW-HackML-2020\lab3\data\\bd\\bd_valid.h5'
     path_bd = 'models/bd_net.h5'
     bd_model = keras.models.load_model(path_bd)
     cl_x_test, cl_y_test = data_loader(clean_data_filename)
+    cl_x_test_raw, _ = data_loader(clean_data_filename)
     bd_x_test, bd_y_test = data_loader(poisoned_data_filename)
+    bd_x_test_raw, _ = data_loader(poisoned_data_filename)
     shift = cl_x_test[0]
-    num_shift = 20
+    num_shift = 5
     for x in cl_x_test[1:num_shift-1]:
         shift += x
     shift = shift / num_shift
     for i, x in enumerate(cl_x_test):
-        cl_x_test[i] = x/num_shift + shift
+        # cl_x_test[i] = x/num_shift + shift
+        cl_x_test[i] = x + shift
+
     for i, x in enumerate(bd_x_test):
-        bd_x_test[i] = x/num_shift + shift
+        # bd_x_test[i] = x/num_shift + shift
+        bd_x_test[i] = x + shift
     y1 = bd_model.predict(cl_x_test)
     y2 = bd_model.predict(bd_x_test)
     print(min([v.max() for v in y1]))
@@ -37,4 +63,5 @@ if __name__ == '__main__':
     plt.hist(data1, **kwargs, color='r', label='Ideal')
     plt.hist(data2, **kwargs, color='g', label='Fair')
     plt.show()
+    eval()
     print('debug')
